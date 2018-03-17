lieuwex/doomrooms

commit 762ae0bfe1b4dc18b77ce4d04f158f067a7bf48e

make gameserver listener code less hacky

config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"doomrooms/communicators"
-	"doomrooms/connections"
 	"fmt"
 	"io/ioutil"
 	"strconv"
@@ -12,6 +11,7 @@ import (
 )
 
 type listenerSetting struct {
+	Type    string
 	Host    string
 	Port    uint64
 	Timeout uint
@@ -23,13 +23,17 @@ func HandleService(cm *communicators.CommunicatorManager, service string, settin
 	}
 	portStr := strconv.FormatUint(settings.Port, 10)
 
-	var err error
-	if service == "gameserver-tcp-json" {
-		err = connections.ListenGameservers(settings.Host, portStr)
-	} else {
-		err = cm.StartService(service, settings.Host, portStr)
+	var isPlayer bool
+	switch settings.Type {
+	case "player":
+		isPlayer = true
+	case "gameserver":
+		isPlayer = false
+	default:
+		return fmt.Errorf("unknown type '%s'", settings.Type)
 	}
 
+	err := cm.StartService(service, settings.Host, portStr, isPlayer)
 	if err != nil {
 		return err
 	}
@@ -51,16 +55,18 @@ func ReadConfig(cm *communicators.CommunicatorManager, filename string) error {
 		return err
 	}
 
-	var config map[string]listenerSetting
+	var config map[string][]listenerSetting
 	if _, err := toml.Decode(string(bytes), &config); err != nil {
 		return err
 	}
 
 	for service, settings := range config {
-		err = HandleService(cm, service, settings)
-		if err != nil {
-			// REVIEW
-			return err
+		for _, setting := range settings {
+			err = HandleService(cm, service, setting)
+			if err != nil {
+				// REVIEW
+				return err
+			}
 		}
 	}
 

communicators/communicator.go
@@ -12,39 +12,49 @@ import (
 
 type CommunicatorManager struct {
 	Log           *logrus.Logger
-	Communicators map[string]types.Communicator
+	Communicators []types.Communicator
 
-	connCh chan *connections.Connection
+	playerConnCh     chan *connections.Connection
+	gameServerConnCh chan *connections.Connection
 }
 
 func MakeCommunicatorManager() *CommunicatorManager {
 	cm := &CommunicatorManager{
-		connCh: make(chan *connections.Connection),
-		Communicators: map[string]types.Communicator{
-			"player-tcp-json": json.MakeTCPJSONCommunicator(),
-			"player-ws-json":  json.MakeWebsocketJSONCommunicator(),
-		},
-		Log: logrus.New(),
+		playerConnCh:     make(chan *connections.Connection),
+		gameServerConnCh: make(chan *connections.Connection),
+		Log:              logrus.New(),
 	}
 	cm.Log.Formatter = utils.Formatter
 	return cm
 }
 
-func (cm *CommunicatorManager) StartService(service string, host string, port string) error {
-	comm := cm.Communicators[service]
-	if comm == nil {
+func (cm *CommunicatorManager) StartService(service string, host string, port string, isPlayer bool) error {
+	var comm types.Communicator
+	switch service {
+	case "tcp-json":
+		comm = json.MakeTCPJSONCommunicator()
+	case "ws-json":
+		comm = json.MakeWebsocketJSONCommunicator()
+	default:
 		return fmt.Errorf("no service with name '%s' found", service)
 	}
+
+	cm.Communicators = append(cm.Communicators, comm)
 
 	err := comm.Start(host, port)
 	if err != nil {
 		return err
 	}
 
+	connCh := cm.gameServerConnCh
+	if isPlayer {
+		connCh = cm.playerConnCh
+	}
+
 	go func() {
 		for {
 			netConn := <-comm.ConnectionCh()
-			cm.connCh <- connections.MakeConnection(netConn)
+			connCh <- connections.MakeConnection(netConn)
 		}
 	}()
 
@@ -61,6 +71,9 @@ func (cm *CommunicatorManager) StopServices() error {
 	return nil
 }
 
-func (cm *CommunicatorManager) ConnectionCh() <-chan *connections.Connection {
-	return cm.connCh
+func (cm *CommunicatorManager) PlayerConnectionCh() <-chan *connections.Connection {
+	return cm.playerConnCh
+}
+func (cm *CommunicatorManager) GameServerConnectionCh() <-chan *connections.Connection {
+	return cm.gameServerConnCh
 }

connections/serverApi.go
@@ -3,7 +3,6 @@ package connections
 import (
 	"fmt"
 
-	"doomrooms/communicators/json"
 	"doomrooms/types"
 
 	log "github.com/sirupsen/logrus"
@@ -76,36 +75,19 @@ func removeGameServer(gs *GameServer) error {
 	return nil
 }
 
-func ListenGameservers(host string, port string) error {
-	comm := json.MakeTCPJSONCommunicator()
-	err := comm.Start(host, port)
-	if err != nil {
-		return err
-	}
-
-	go func() {
-		for {
-			netConn := <-comm.ConnectionCh()
-			gs := &GameServer{
-				Connection: MakeConnection(netConn),
-				NotifyOptions: map[string]string{
-					"room-creation": "on",
-					"room-join":     "off",
-					"room-leave":    "off",
-
-					"game-start": "on", // why would anyone want to set this to "off"?
-				},
-			}
-			go HandleGameServer(gs)
-		}
-	}()
-
-	return nil
-}
-
-func HandleGameServer(gs *GameServer) {
-	conn := gs.Connection
+func HandleGameServerConnection(conn *Connection) {
 	defer conn.Close()
+
+	gs := &GameServer{
+		Connection: conn,
+		NotifyOptions: map[string]string{
+			"room-creation": "on",
+			"room-join":     "off",
+			"room-leave":    "off",
+
+			"game-start": "on", // why would anyone want to set this to "off"?
+		},
+	}
 
 	addGameServer(gs)
 	defer removeGameServer(gs)

main.go
@@ -62,11 +62,25 @@ func main() {
 		}).Fatal("error while reading config")
 	}
 
-	for {
-		connection := <-cm.ConnectionCh()
-		log.WithFields(log.Fields{
-			"conn": connection,
-		}).Info("got new connection in main.go")
-		go connections.HandlePlayerConnection(connection)
-	}
+	go func() {
+		for {
+			connection := <-cm.PlayerConnectionCh()
+			log.WithFields(log.Fields{
+				"conn": connection,
+			}).Info("got new player connection in main.go")
+			go connections.HandlePlayerConnection(connection)
+		}
+	}()
+
+	go func() {
+		for {
+			connection := <-cm.GameServerConnectionCh()
+			log.WithFields(log.Fields{
+				"conn": connection,
+			}).Info("got new game server connection in main.go")
+			go connections.HandleGameServerConnection(connection)
+		}
+	}()
+
+	select {}
 }

commit 9c561ef107742611f3f9554881fa90b115dff9ac

remove broken unused communicator warning after 762ae0bfe1b4dc18b77ce4d04f158f067a7bf48e

config/config.go
@@ -70,13 +70,5 @@ func ReadConfig(cm *communicators.CommunicatorManager, filename string) error {
 		}
 	}
 
-	for name, comm := range cm.Communicators {
-		if !comm.Started() {
-			cm.Log.WithFields(log.Fields{
-				"service": name,
-			}).Warn("service not enabled")
-		}
-	}
-
 	return nil
 }