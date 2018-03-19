54a336d48320255cb76a658faf38338da72ec6d0
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-03-19 18:49:39 +0100

    player api: use return instead of reply for command handlers

--- connections/playerCommands.go
+++ connections/playerCommands.go
@@ -5,10 +5,6 @@ import "doomrooms/types"
 func onPlayerCommand(player *Player, conn *Connection, msg *types.Message) {
 	handled := false
 
-	reply := func(err string, res interface{}) {
-		conn.Reply(msg.ID, err, res)
-	}
-
 	roomOthers := func() []*Player {
 		res := player.CurrentRoom().Players[:0]
 		for _, p := range player.CurrentRoom().Players {
@@ -19,91 +15,89 @@ func onPlayerCommand(player *Player, conn *Connection, msg *types.Message) {
 		return res
 	}
 
-	handleCommand := func(method string, argCount int, fn func()) {
+	handleCommand := func(method string, argCount int, fn func() (interface{}, string)) {
 		if msg.Method != method {
 			return
 		}
 		handled = true
 
 		if len(msg.Args) != argCount && argCount != -1 {
-			reply("not enough arguments", nil)
+			conn.Reply(msg.ID, "not enough arguments", nil)
 			return
 		}
 
-		fn()
+		res, err := fn()
+		conn.Reply(msg.ID, err, res)
 	}
 
-	handleGameCommand := func(method string, argCount int, fn func()) {
-		handleCommand(method, argCount, func() {
+	handleGameCommand := func(method string, argCount int, fn func() (interface{}, string)) {
+		handleCommand(method, argCount, func() (interface{}, string) {
 			if player.Game() == nil {
-				reply("no game set", nil)
-				return
+				return nil, "no game set"
 			}
 
-			fn()
+			return fn()
 		})
 	}
 
-	handleRoomCommand := func(method string, argCount int, fn func()) {
-		handleGameCommand(method, argCount, func() {
+	handleRoomCommand := func(method string, argCount int, fn func() (interface{}, string)) {
+		handleGameCommand(method, argCount, func() (interface{}, string) {
 			if player.CurrentRoom() == nil {
-				reply("not in a room", nil)
-				return
+				return nil, "not in a room"
 			}
 
-			fn()
+			return fn()
 		})
 	}
 
-	handleCommand("set-game", 1, func() {
+	handleCommand("set-game", 1, func() (interface{}, string) {
 		gameID := msg.Args[0].(string)
 
 		g := GetGame(gameID)
 		if g == nil {
-			reply("game not found", nil)
+			return nil, "game not found"
 		}
 
 		player.CurrentGameID = gameID
-		reply("", g)
+		return g, ""
 	})
 
-	handleCommand("send-private-chat", 2, func() {
+	handleCommand("send-private-chat", 2, func() (interface{}, string) {
 		nick := msg.Args[0].(string)
 		line := msg.Args[1].(string)
 
 		target := GetPlayer(nick)
 		if target == nil {
-			reply("player-not-found", nil)
-			return
+			return nil, "player-not-found"
 		}
 
 		target.Emit("private-chat", player.Nickname, line)
-		reply("", nil)
+		return nil, ""
 	})
 
-	handleCommand("get-tags", 0, func() { // REVIEW
-		reply("", player.Tags)
+	handleCommand("get-tags", 0, func() (interface{}, string) { // REVIEW
+		return player.Tags, ""
 	})
 
-	handleCommand("set-tags", 1, func() {
+	handleCommand("set-tags", 1, func() (interface{}, string) {
 		tags := msg.Args[0].(map[string]interface{})
 
 		player.Tags = tags
-		reply("", tags)
+		return tags, ""
 	})
 
-	handleGameCommand("open-pipe", 0, func() {
+	handleGameCommand("open-pipe", 0, func() (interface{}, string) {
 		gs := player.Game().GameServer()
 		ps := MakePipeSession()
 		gs.Emit("pipe-opened", player, ps.PrivateID)
-		reply("", ps.PrivateID)
+		return ps.PrivateID, ""
 	})
 
-	handleGameCommand("get-current-room", 0, func() {
-		reply("", player.CurrentRoom())
+	handleGameCommand("get-current-room", 0, func() (interface{}, string) {
+		return player.CurrentRoom(), ""
 	})
 
-	handleGameCommand("make-room", 3, func() {
+	handleGameCommand("make-room", 3, func() (interface{}, string) {
 		name := msg.Args[0].(string)
 		hidden := msg.Args[1].(bool)
 		options := msg.Args[2].(map[string]interface{})
@@ -114,16 +108,15 @@ func onPlayerCommand(player *Player, conn *Connection, msg *types.Message) {
 		room.AddPlayer(player)
 
 		game.gameServer.Emit("room-creation", room)
-		reply("", room)
-
 		player.CurrentRoomID = room.ID
+
+		return room, ""
 	})
 
-	handleGameCommand("join-room", -1, func() {
+	handleGameCommand("join-room", -1, func() (interface{}, string) {
 		nargs := len(msg.Args)
 		if nargs == 0 {
-			reply("not-enough-args", nil)
-			return
+			return nil, "not-enough-args"
 		}
 
 		id := msg.Args[0].(string)
@@ -134,52 +127,50 @@ func onPlayerCommand(player *Player, conn *Connection, msg *types.Message) {
 
 		room := player.Game().GetRoom(id)
 		if room == nil {
-			reply("room not found", nil)
-			return
+			return nil, "room not found"
 		}
 
 		if room.Hidden && !room.PlayerInvited(player) {
-			reply("not-invited", nil)
-			return
+			return nil, "not-invited"
 		}
 
 		if pass := room.Options["password"]; pass != nil && givenPass != pass {
-			reply("incorrect-password", nil)
-			return
+			return nil, "incorrect-password"
 		}
 
 		err := room.AddPlayer(player)
 		if err != nil {
-			reply(err.Error(), nil)
-			return
+			return nil, err.Error()
 		}
 
 		player.Game().gameServer.Emit("room-join", room, player)
-		reply("", room)
-
 		player.CurrentRoomID = room.ID
+
+		return room, ""
 	})
 
-	handleGameCommand("get-room", 1, func() {
+	handleGameCommand("get-room", 1, func() (interface{}, string) {
 		id := msg.Args[0].(string)
 		room := player.Game().GetRoom(id)
-		reply("", room)
+		return room, ""
 	})
 
-	handleGameCommand("search-rooms", 1, func() {
+	handleGameCommand("search-rooms", 1, func() (interface{}, string) {
 		query := msg.Args[0].(string)
 		rooms := player.Game().SearchRooms(query, false)
-		reply("", rooms)
+		return rooms, ""
 	})
 
-	handleRoomCommand("send-room-chat", 1, func() {
+	handleRoomCommand("send-room-chat", 1, func() (interface{}, string) {
 		line := msg.Args[0].(string)
 
 		for _, p := range roomOthers() {
 			p.Emit("room-chat", player.Nickname, line)
 		}
+
+		return nil, ""
 	})
-	handleRoomCommand("send-filtered-room-chat", 2, func() {
+	handleRoomCommand("send-filtered-room-chat", 2, func() (interface{}, string) {
 		line := msg.Args[0].(string)
 		filter := msg.Args[1].(map[string]interface{})
 
@@ -190,81 +181,76 @@ func onPlayerCommand(player *Player, conn *Connection, msg *types.Message) {
 
 			p.Emit("filtered-room-chat", player.Nickname, line, filter)
 		}
+
+		return nil, ""
 	})
 
-	handleRoomCommand("invite-player", 1, func() {
+	handleRoomCommand("invite-player", 1, func() (interface{}, string) {
 		nick := msg.Args[0].(string)
 		p := GetPlayer(nick)
 		if p == nil {
-			reply("player-not-found", nil)
-			return
+			return nil, "player-not-found"
 		}
 
 		player.CurrentRoom().InvitePlayer(player, p)
-		reply("", nil)
+		return nil, ""
 	})
 
-	handleRoomCommand("kick-player", 2, func() {
+	handleRoomCommand("kick-player", 2, func() (interface{}, string) {
 		nick := msg.Args[0].(string)
 		reason := msg.Args[1].(string)
 		room := player.CurrentRoom()
 
 		if room.Admin != player {
-			reply("not-admin", nil)
-			return
+			return nil, "not-admin"
 		}
 
 		p := GetPlayer(nick)
 		if p == nil {
-			reply("player-not-found", nil)
-			return
+			return nil, "player-not-found"
 		}
 
 		p.Emit("kick", reason)
 		room.RemovePlayer(p)
 
-		reply("", nil)
+		return nil, ""
 	})
 
-	handleRoomCommand("leave-room", 0, func() {
+	handleRoomCommand("leave-room", 0, func() (interface{}, string) {
 		err := player.CurrentRoom().RemovePlayer(player)
 		if err != nil {
-			reply(err.Error(), nil)
-			return
+			return nil, err.Error()
 		}
 
-		reply("", nil)
+		return nil, ""
 	})
 
-	handleRoomCommand("start", 0, func() {
+	handleRoomCommand("start", 0, func() (interface{}, string) {
 		room := player.CurrentRoom()
 
 		if room.Admin != player {
-			reply("not-admin", nil)
-			return
+			return nil, "not-admin"
 		}
 
 		err := room.Start()
 		if err != nil {
-			reply(err.Error(), nil)
-			return
+			return nil, err.Error()
 		}
 
-		reply("", room)
+		return room, ""
 	})
 
-	handleRoomCommand("message-game-server", -1, func() {
+	handleRoomCommand("message-game-server", -1, func() (interface{}, string) {
 		gs := player.Game().gameServer
 		res, err := gs.Send("player-message", msg.Args...)
 		if err != nil {
-			reply(err.Error(), nil)
-			return
+			return nil, err.Error()
 		}
 
-		reply("", res)
+		return res, ""
 	})
 
 	if !handled {
-		reply("unknown command", nil)
+		conn.Reply(msg.ID, "unknown command", nil)
 	}
 }
